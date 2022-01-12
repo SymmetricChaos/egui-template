041f95fb5658e4ecb9fd6de89a6e31eb9733e031
SymmetricChaos <ajfraebel@gmail.com>
2022-01-12 09:56:22 -0500

    removed web stuff learning the ui

--- src/app.rs
+++ src/app.rs
@@ -1,23 +1,30 @@
 use eframe::{egui, epi};
 
+pub fn gcd_fn(a: u64, b: u64) -> u64 {
+    let mut a = a;
+    let mut b = b;
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a
+}
+
+
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 //#[cfg_attr(feature = "persistence", derive(serde::Deserialize, serde::Serialize))]
 //#[cfg_attr(feature = "persistence", serde(default))] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    // Example stuff:
-    label: String,
-
-    // this how you opt-out of serialization of a member
-    //#[cfg_attr(feature = "persistence", serde(skip))]
-    value: f32,
+    a: u64,
+    b: u64,
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
         Self {
-            // Example stuff:
-            label: "Hello World!".to_owned(),
-            value: 2.7,
+            a: 1,
+            b: 1,
         }
     }
 }
@@ -34,25 +41,12 @@ impl epi::App for TemplateApp {
         _frame: &epi::Frame,
         _storage: Option<&dyn epi::Storage>,
     ) {
-        // Load previous app state (if any).
-        // Note that you must enable the `persistence` feature for this to work.
-/*         #[cfg(feature = "persistence")]
-        if let Some(storage) = _storage {
-            *self = epi::get_value(storage, epi::APP_KEY).unwrap_or_default()
-        } */
     }
-
-    /// Called by the frame work to save state before shutdown.
-    /// Note that you must enable the `persistence` feature for this to work.
-/*     #[cfg(feature = "persistence")]
-    fn save(&mut self, storage: &mut dyn epi::Storage) {
-        epi::set_value(storage, epi::APP_KEY, self);
-    } */
 
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::CtxRef, frame: &epi::Frame) {
-        let Self { label, value } = self;
+        let Self { a, b} = self;
 
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -71,48 +65,39 @@ impl epi::App for TemplateApp {
         });
 
         egui::SidePanel::left("side_panel").show(ctx, |ui| {
-            ui.heading("Side Panel");
+            ui.heading("Panel");
 
             ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(label);
+                ui.label("a = ");
+                ui.add(egui::DragValue::new(a).speed(1.0));
             });
 
-            ui.add(egui::Slider::new(value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                *value += 1.0;
-            }
-
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                ui.horizontal(|ui| {
-                    ui.spacing_mut().item_spacing.x = 0.0;
-                    ui.label("powered by ");
-                    ui.hyperlink_to("egui", "https://github.com/emilk/egui");
-                    ui.label(" and ");
-                    ui.hyperlink_to("eframe", "https://github.com/emilk/egui/tree/master/eframe");
-                });
+            ui.horizontal(|ui| {
+                ui.label("b = ");
+                ui.add(egui::DragValue::new(b).speed(1.0));
             });
+
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
 
-            ui.heading("eframe template");
-            ui.hyperlink("https://github.com/emilk/eframe_template");
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/master/",
-                "Source code."
-            ));
+            ui.horizontal(|ui| {
+                ui.label("gcd(a,b) = ");
+                ui.label(gcd_fn(*a,*b).to_string());
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("a + b = ");
+                ui.label((*a+*b).to_string());
+            });
+
+            ui.horizontal(|ui| {
+                ui.label("a * b = ");
+                ui.label((*a**b).to_string());
+            });
             egui::warn_if_debug_build(ui);
         });
 
-        if false {
-            egui::Window::new("Window").show(ctx, |ui| {
-                ui.label("Windows can be moved by dragging them.");
-                ui.label("They are automatically sized based on contents.");
-                ui.label("You can turn on resizing and scrolling if you like.");
-                ui.label("You would normally chose either panels OR windows.");
-            });
-        }
     }
 }
